8f5089e863866a06d2cb422a081e585322729894
thientho03 <128447925+thientho03@users.noreply.github.com>
2023-11-18 22:31:34 +0700

    Add fundamental features

---
+++ resident/static/js/map.js
@@ -0,0 +1,13 @@
+import search from "./search.js";
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 10.762622, lng: 106.660172 },
+        zoom: 13,
+        mapTypeControl: false,
+    });
+
+    search(map);
+}
+
+window.initMap = initMap;
