5cfbbdde4f74a6d25e23a606f3c230544634a6f8
Kha7734 <huynhsikha2003@gmail.com>
2023-11-24 21:23:24 +0700

    Implement search-place bar

--- resident/static/js/search.js
+++ resident/static/js/search.js
@@ -1,13 +1,9 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 function search(map) {
   // Create the search box and link it to the UI element.
   const input = document.getElementById('pac-input');
   const searchBox = new google.maps.places.SearchBox(input);
 
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', () => {
     searchBox.setBounds(map.getBounds());
