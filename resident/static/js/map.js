ae956bd5f5f3f3a9b8264b616c895906af61644c
Kha7734 <huynhsikha2003@gmail.com>
2023-11-26 18:32:03 +0700

    "Enhance Information Address"

--- resident/static/js/map.js
+++ resident/static/js/map.js
@@ -1,17 +1,18 @@
-import search from "./search.js";
-import { setMarker } from './marker.js'
-import btnCurrentPosition from "./btnCurrentPosition.js";
+import search from './search.js';
+import { setMarker } from './marker.js';
+import btnCurrentPosition from './btnCurrentPosition.js';
 
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 10.762622, lng: 106.660172 },
-        zoom: 13,
-        mapTypeControl: false,
-    });
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 10.762622, lng: 106.660172 },
+    zoom: 13,
+    mapTypeControl: false,
+  });
+  const geocoder = new google.maps.Geocoder();
 
-    search(map);
-    setMarker(map);
-    btnCurrentPosition(map);
+  search(map, geocoder);
+  setMarker(map);
+  btnCurrentPosition(map);
 }
 
 window.initMap = initMap;
