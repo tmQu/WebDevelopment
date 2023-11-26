aec716b346576f605ec119375938f43d8958b5b1
thientho03 <128447925+thientho03@users.noreply.github.com>
2023-11-26 16:50:28 +0700

    Add Button Current Position

--- resident/static/js/map.js
+++ resident/static/js/map.js
@@ -1,5 +1,7 @@
 import search from "./search.js";
-import {setMarker} from './marker.js'
+import { setMarker } from './marker.js'
+import btnCurrentPosition from "./btnCurrentPosition.js";
+
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 10.762622, lng: 106.660172 },
@@ -9,6 +11,7 @@ function initMap() {
 
     search(map);
     setMarker(map);
+    btnCurrentPosition(map);
 }
 
 window.initMap = initMap;
