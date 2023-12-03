a98ccdbc9fe965b51f57babdffffde71c0598336
hskha23 <huynhsikha2003@gmail.com>
2023-12-03 12:26:12 -0800

    Configure the cloud database connection

--- cadre/app.js
+++ cadre/app.js
@@ -1,67 +1,67 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import billboardRouter from './routers/billboardRouter.js'
-import cors from 'cors'
-const app = express()
+import billboardRouter from './routers/billboardRouter.js';
+import cors from 'cors';
+
+const app = express();
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful!'));
+
 app.use(cors());
 
 app.use('/billboard', billboardRouter);
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const dbName = "advertisement"
-mongoose.connect('mongodb://127.0.0.1:27017/' + dbName).catch(error => console.log(error));;
-
-
-
 var datatest = {
-    id: "BB",
-    location: {
-        lat: 10.76320512569573,
-        lng: 106.68307353233895
+  id: 'BB',
+  location: {
+    lat: 10.76320512569573,
+    lng: 106.68307353233895,
+  },
+  addr: {
+    NumStreet: '235 Đ. Nguyễn Văn Cừ',
+    ward: 'Phường Nguyễn Cư Trinh',
+    district: 'Quận 1',
+    city: 'Thành phố Hồ Chí Minh',
+  },
+  isPlan: true,
+  advertisementForm: 'Cổ động chính trị',
+  locationCategory: 'Đất công/Công viên/Hành lang an toàn giao thông',
+  billboards: [
+    {
+      billboardType: 'Trụ cụm pano',
+      quatity: '1 trụ/bảng',
+      size: {
+        width: 2.5,
+        height: 10,
+      },
+      img: ['./test.png', './test.png', './test.png'],
+      date_expired: Date('28/11/2023'),
     },
-    addr : {
-        NumStreet: '235 Đ. Nguyễn Văn Cừ',
-        ward: 'Phường Nguyễn Cư Trinh',
-        district: 'Quận 1',
-        city: 'Thành phố Hồ Chí Minh'    
+    {
+      billboardType: 'Trụ cụm pano',
+      quatity: '2 trụ/bảng',
+      size: {
+        width: 2.5,
+        height: 10,
+      },
+      img: ['./test.png', './test.png', './test.png'],
+      date_expired: Date('28/11/2023'),
     },
-    isPlan: true,
-    advertisementForm: 'Cổ động chính trị',
-    locationCategory: 'Đất công/Công viên/Hành lang an toàn giao thông',
-    billboards: [
-        {
-            billboardType: 'Trụ cụm pano',
-            quatity: '1 trụ/bảng',
-            size: {
-                width: 2.5,
-                height: 10
-            },
-            img: [
-                './test.png',
-                './test.png',
-                './test.png'
-            ],
-            date_expired: Date('28/11/2023')
-        },
-        {
-            billboardType: 'Trụ cụm pano',
-            quatity: '2 trụ/bảng',
-            size: {
-                width: 2.5,
-                height: 10
-            },
-            img: [
-                './test.png',
-                './test.png',
-                './test.png'
-            ],
-            date_expired: Date('28/11/2023')
-        }
-    ]
-}
+  ],
+};
 
 // import BillboardLocation from './model/billboardLocation.js';
 // var locationTest = [
@@ -90,6 +90,9 @@ var datatest = {
 //         console.log(err);
 //     }
 // }
-app.listen(4000, () => {
-    console.log('Server running on port 4000')
-})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
