52f20a5d6e4dcc9f159f061ce1d21ea84b86af36
hskha23 <huynhsikha2003@gmail.com>
2023-12-10 13:04:16 +0700

    Completely Authentication, Authorization and Security implementation

--- cadre/app.js
+++ cadre/app.js
@@ -1,24 +1,59 @@
 import express from 'express';
 import morgan from 'morgan';
-import boardRouter from './routes/boardRoutes.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import cors from 'cors'
+import cors from 'cors';
+import rateLimit from 'express-rate-limit'; // Limit requests from same API (prevent from DDOS)
+import helmet from 'helmet'; // Set security HTTP headers
+import mongoSanitize from 'express-mongo-sanitize'; // Data sanitization against NoSQL query injection
+import xss from 'xss-clean'; // Data sanitization against XSS
 
+import boardRouter from './routes/boardRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
 app.use(cors());
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-console.log(process.env.NODE_ENV);
+// 1) GLOBAL MIDDLEWARES
+app.use(helmet()); // Set security HTTP headers
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same API (prevent from DDOS)
+const limiter = rateLimit({
+  max: 100, // 100 requests from same IP
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
 
-app.use(express.static(`${__dirname}/public`));
+app.use('/api', limiter); // apply to all routes that start with /api
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+// clear all the query string from malicious characters
+// {email: $gt: ""}, {password: "12345678"}
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Serving static files
+app.use(express.static(`${__dirname}/static`));
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
 app.use('/api/v1/boards', boardRouter);
+app.use('/api/v1/users', userRouter);
 
 export default app;
--- cadre/controllers/userController.js
+++ cadre/controllers/userController.js
@@ -1,48 +1,139 @@
-import User from "../models/UserModel";
+import User from '../models/userModel.js';
+import AppError from '../utils/appError.js';
+import catchAsync from '../utils/catchAsync.js';
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
-
-var userController = {
-    getAll: async (req, res) => {
-        try {
-            var result = await User.find({},{pwd: -1})
-            res.json(result);
-        }
-        catch(err){
-            res.send(err);
-        }
-    },
-    getById: async (req, res) => {
-        try{
-            var id = req.query.id;
-            var result = await BillboardLocation.find({idUser: id}, {pwd: -1});
-            res.json(result[0]);
-        }
-        catch(err){
-            res.send(err);
-        }
-    },
-    addUser: async (req, res) => {
-        try {
-            infor = req.body;
-            // ? pwd
-
-            res.send('ok');
-        }
-        catch(err)
-        {
-            res.send(err);
-        }
-    },
-    deleteUser: async (req, res) => {
-        try {
-            var result = await User.findAndDelete({idUser: req.query.id})
-        
-        }
-        catch(err){
-            res.send(err);
-        }
+const userController = {
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+          users,
+        },
+      });
+    } catch (err) {
+      res.status(404).json({
+        status: 'fail',
+        message: err,
+      });
     }
-}
+  },
+  getUser: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      res.status(200).json({
+        status: 'success',
+        data: {
+          user,
+        },
+      });
+    } catch (err) {
+      res.status(404).json({
+        status: 'fail',
+        message: err,
+      });
+    }
+  },
+  createUser: async (req, res) => {
+    try {
+      const newUser = await User.create(req.body);
+      res.status(201).json({
+        status: 'success',
+        data: {
+          user: newUser,
+        },
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: 'fail',
+        message: err,
+      });
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      res.status(200).json({
+        status: 'success',
+        data: {
+          user,
+        },
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: 'fail',
+        message: err,
+      });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    } catch (err) {
+      res.status(404).json({
+        status: 'fail',
+        message: err,
+      });
+    }
+  },
+  updateMe: catchAsync(async (req, res, next) => {
+    // 1 Create error if user POSTs password data
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(
+        new AppError(
+          'This route is not for password updates. Please use /updatePassword',
+          400
+        )
+      );
+    }
 
-export default userController
+    // 2 Filtered out unwanted fields names that are not allowed to be updated
+    const filterBody = filterObj(
+      req.body,
+      'name',
+      'email',
+      'phoneNumber',
+      'dateOfBirth'
+    );
+
+    // 3 Update user document
+    const updatedUser = await User.findByIdAndUpdate(req.user._id, filterBody, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser,
+      },
+    });
+  }),
+  deleteMe: async (req, res, next) => {
+    await User.findByIdAndUpdate(req.user._id, { active: false });
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  },
+};
+
+export default userController;
